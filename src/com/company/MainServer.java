838e502b81987fbcd8787e23f1ebc12ee4d52e85
forestfox128 <basiab217@gmail.com>
2017-12-08 16:40:34 +0100

    Basics of the server

---
+++ src/com/company/MainServer.java
@@ -0,0 +1,82 @@
+package com.company;
+
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class MainServer {
+
+    private ServerSocket server = null;
+    private Socket client = null;
+    private BufferedReader in = null;
+    private static PrintWriter out = null;
+    private String clientName = "";
+    ServerGUI serverWindow;
+    private ArrayList<String> users = new ArrayList<>();
+
+
+
+    private MainServer() {
+        try {
+            server = new ServerSocket(9999);
+            serverWindow = new ServerGUI();
+            serverWindow.startServer();
+
+
+        }
+        catch (IOException e) {
+            System.out.println("Could not listen on port 6666"); System.exit(-1);
+        }
+    }
+
+    private void listenSocket() {
+
+        try {
+            client = server.accept();
+        } catch (IOException e) {
+            System.out.println("Accept failed: 6587");
+            System.exit(-1);
+        }
+        try {
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out = new PrintWriter(client.getOutputStream(), true);
+
+        } catch (IOException e) {
+            System.out.println("Accept failed: 6666");
+            System.exit(-1);
+        }
+
+        while (clientName != null) {
+
+            try {
+
+                clientName = in.readLine();
+                serverWindow.addClient(clientName);
+            }
+
+            catch (IOException e) {
+                System.out.println("Cannot connect");
+                System.exit(-1);
+            }
+        }
+    }
+
+    protected void finalize() {
+        try {
+            in.close();
+            out.close();
+            client.close();
+            server.close();
+        }
+        catch (IOException e) {
+            System.out.println("Could not close."); System.exit(-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        MainServer server = new MainServer();
+        server.listenSocket();
+    }
+}
+
---
+++ src/com/company/ServerGUI.java
@@ -0,0 +1,68 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class ServerGUI {
+
+    JFrame frame = new JFrame("ChineseCheckersServer");
+
+
+    private Label output1;
+    private Label output2;
+    private Label listPlayers;
+    private Label socket;
+
+    //private Socket socket = null;
+
+
+
+    public ServerGUI(){
+        builtWindow();
+    }
+
+    public void builtWindow(){
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(800, 450);
+        frame.setTitle("ChineseCheckersServer");
+        frame.setFont(new Font ("SansSerif",Font.PLAIN, 18));
+
+        socket = new Label("Status");
+        socket.setFont(new Font ("SansSerif",Font.PLAIN, 16));
+        socket.setBounds(30,50,700,50);
+
+        output1 = new Label();
+        output1.setBackground(Color.white);
+        output1.setFont(new Font ("SansSerif",Font.PLAIN, 16));
+        output1.setBounds(30,100,700,50);
+
+
+        listPlayers = new Label("List of players: ");
+        listPlayers.setBounds(30,180,700,30);
+        listPlayers.setFont(new Font ("SansSerif",Font.PLAIN, 16));
+        output2 = new Label();
+        output2.setBackground(Color.white);
+        output2.setBounds(30,220,700,100);
+        output2.setFont(new Font ("SansSerif",Font.PLAIN, 16));
+
+        frame.add(output1);
+        frame.add(output2);
+        frame.add(listPlayers);
+        frame.add(socket);
+    }
+
+    public void startServer(){
+        output1.setText("Server is running");
+    }
+
+    public void addClient(String player){
+
+        output1.setText("New player logged");
+        output2.setText(player);
+    }
+}
+
